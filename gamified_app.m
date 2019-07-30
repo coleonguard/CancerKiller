3e5e167e0a196cb1f6e8aa5f02a06046f1619d6f
Sina-N17 <53063565+Sina-N17@users.noreply.github.com>
2019-07-30 14:39:17 -0700

    Update gamified_app.m
    Delay time works

--- gamified_app.m
+++ gamified_app.m
@@ -15,14 +15,12 @@ classdef gamified_app < matlab.apps.AppBase
 
         % Button pushed function: NormalButton
         function NormalButtonPushed(app, event)
-            global delayTime;
-	    delayTime = 0;
+	        delayTime = 0;
             MasterV1;
         end
 
         % Button pushed function: DelayedButton
         function DelayedButtonPushed(app, event)
-	    global delayTime;
             delayTime = 3;
             MasterV1;
         end
